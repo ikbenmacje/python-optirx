f81bf65e963fb5e6575fd80db51493b776682182
Jørgen Nordmoen <jno@ffi.no>
2014-09-29 11:55:46 +0200

    Updated to support NatNet 2.6
    Updated the code in several places to support NatNet version 2.6. This
    updated should allow optirx to unpack data coming from NatNet 2.6 >=.
    Most of the changes are additions and new features.

--- optirx.py
+++ optirx.py
@@ -80,8 +80,9 @@ RIGIDBODY_FORMAT =  "=i3f4f"
 #   mrk_ids is a list of integers or None (NatNet version < 2.0)
 #   mrk_sizes is a list of floats or None (NatNet version < 2.0)
 #   mrk_mean_error is a float or None (NatNet version < 2.0)
+#   tracking_valid is a boolean or None (NatNet version <= 2.6)
 RigidBody = namedtuple("RigidBody",
-                       "id position orientation markers mrk_ids mrk_sizes mrk_mean_error")
+                       "id position orientation markers mrk_ids mrk_sizes mrk_mean_error tracking_valid")
 
 
 # Skeleton (NetNet >= 2.1) is a collection of rigid bodies:
@@ -89,7 +90,12 @@ Skeleton = namedtuple("Skeleton", "id rigid_bodies")
 
 
 # LabeledMarker (NatNet >= 2.3)
-LabeledMarker = namedtuple("LabeledMarker", "id position size")
+# New in NatNet 2.6 is support for:
+#   occluded, if the marker was not visible in this frame
+#   point_cloud_solved, if the position was provided by point cloud solver
+#   model_solved, if the position was provided by model solve
+# if version is older than 2.6 all values named above will be None
+LabeledMarker = namedtuple("LabeledMarker", "id position size occluded point_cloud_solved model_solved")
 
 
 # frame payload format (PacketClient.cpp:537) cannot be unpacked by
@@ -114,8 +120,11 @@ LabeledMarker = namedtuple("LabeledMarker", "id position size")
 #  - LABELED_MARKERS (...), ver >= 2.3
 #  - latency (float),
 #  - timecode (int, int),
+#  - timestamp (float), version >= 2.6(?),
+#  - is_recording (boolean), version >= 2.6(?),
+#  - tracked_models_changed (boolean), version >= 2.6(?),
 #  - end of data tag (int).
-FrameOfData = namedtuple("FrameOfData", "frameno sets other_markers rigid_bodies skeletons labeled_markers latency timecode")
+FrameOfData = namedtuple("FrameOfData", "frameno sets other_markers rigid_bodies skeletons labeled_markers latency timecode timestamp is_recording tracked_models_changed")
 
 
 # type can be one of DATASET_MARKERSET, DATASET_RIGIDBODY, DATASET_SKELETON
@@ -198,15 +207,23 @@ def _unpack_rigid_bodies(data, version):
             mrk_ids, data = _unpack_head(str(nmarkers) + "i", data)
             mrk_sizes, data = _unpack_head(str(nmarkers) + "f", data)
             (mrk_mean_error,), data = _unpack_head("f", data)
+            tracking_valid = None
+            if _version_is_at_least(version, 2, 6):
+                #New in version 2.6 is support for telling if the rigid body
+                #was successfully tracked
+                (params,), data = _unpack_head("h", data)
+                tracking_valid = params & 0x01
         else:
             mrk_ids, mrk_sizes, mrk_mean_error = None, None, None
+            tracking_valid = None
         rb = RigidBody(id=rbid,
                        position=(x,y,z),
                        orientation=(qx,qy,qz,qw),
                        markers=markers,
                        mrk_ids=mrk_ids,
                        mrk_sizes=mrk_sizes,
-                       mrk_mean_error=mrk_mean_error)
+                       mrk_mean_error=mrk_mean_error,
+                       tracking_valid=tracking_valid)
         rbodies.append(rb)
     return rbodies, data
 
@@ -229,9 +246,22 @@ def _unpack_labeled_markers(data, version):
         return [], data
     (nmarkers,), data = _unpack_head("i", data)
     lmarkers = []
-    for i in xrange(nmarkers):
-        (id,x,y,z,size), data = _unpack_head("i4f", data)
-        lmarkers.append(LabeledMarker(id, (x, y, z), size))
+    if _version_is_at_least(version, 2, 6):
+        #Duplicate looping code to avoid an if check every
+        #loop iteration
+        for _ in xrange(nmarkers):
+            (id, x, y, z, size, params), data = _unpack_head("i4fh", data)
+            #New in version 2.6, PacketClient.cpp 753
+            occluded = params & 0x01
+            pc_solved = params & 0x02
+            model_solved = params & 0x04
+            lmarkers.append(LabeledMarker(id, (x, y, z), size, occluded,
+                pc_solved, model_solved))
+    else:
+        for _ in xrange(nmarkers):
+            (id, x, y, z, size), data = _unpack_head("i4f", data)
+            lmarkers.append(LabeledMarker(id, (x, y, z), size, 
+                None, None, None))
     return lmarkers, data
 
 
@@ -248,7 +278,21 @@ def _unpack_frameofdata(data, version):
     bodies, data = _unpack_rigid_bodies(data, version)
     skels, data = _unpack_skeletons(data, version)
     lmarkers, data = _unpack_labeled_markers(data, version)
-    (latency,timecode1,timecode2,eod), data = _unpack_head("fIIi", data)
+    if _version_is_at_least(version, 2, 6):
+        # In the latest version of PacketClient.cpp several new parameters
+        # have been added at the end with no version checking, since version
+        # 2.5 did not have these parameters the code here have been added in
+        # an if statement
+        (latency, timecode, timecode_sub, timestamp, params), data = _unpack_head(
+                "fIIfh", data)
+        is_recording = params & 0x01
+        tracked_models_changed = params & 0x02
+    else:
+        (latency, timecode, timecode_sub), data = _unpack_head(
+                "fII", data)
+        is_recording = None
+        tracked_models_changed = None
+    (eod,), data = _unpack_head("i", data)
     assert eod == 0, "End-of-data marker is not 0."
     fod = FrameOfData(frameno=frameno,
                       sets=sets,
@@ -257,7 +301,10 @@ def _unpack_frameofdata(data, version):
                       skeletons=skels,
                       labeled_markers=lmarkers,
                       latency=latency,
-                      timecode=(timecode1, timecode2))
+                      timecode=(timecode, timecode_sub),
+                      timestamp=timestamp,
+                      is_recording=is_recording,
+                      tracked_models_changed=tracked_models_changed)
     return fod, data
 
 
