f1bcdfcab02479e40f6cc28a471bf3e666954d5e
fsuarez6 <fsuarez6@gmail.com>
2015-04-21 16:59:28 +0800

    Improved optirx_demo script

--- optirx_demo.py
+++ optirx_demo.py
@@ -1,4 +1,4 @@
-"""OptiRX demo: connect to Optitrack on the same machine, print received data.
+'''OptiRX demo: connect to Optitrack on the same machine, print received data.
 
 Usage:
 
@@ -6,29 +6,39 @@ Usage:
 
 where natnet_version is 2500, 2600, 2700 etc
 for Motive 1.5, 1.6 betas, and 1.7.x respectively.
-"""
+'''
 
 
 from __future__ import print_function
 import optirx as rx
 import sys
+# For getting the ip address
+import socket, fcntl, struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 
 def demo_recv_data():
     # pretty-printer for parsed
     try:
         from simplejson import dumps, encoder
-        encoder.FLOAT_REPR = lambda o: ("%.4f" % o)
+        encoder.FLOAT_REPR = lambda o: ('%.4f' % o)
     except ImportError:
         from json import dumps, encoder
-        encoder.FLOAT_REPR = lambda o: ("%.4f" % o)
+        encoder.FLOAT_REPR = lambda o: ('%.4f' % o)
 
     # the first optional command line argument:
     # if given, the number of packets to dump
     if sys.argv[1:]:
         max_count = int(sys.argv[1])
     else:
-        max_count = float("inf")
+        max_count = float('inf')
 
     # the second optional command line argument
     # is the version string of the NatNet server;
@@ -38,19 +48,24 @@ def demo_recv_data():
         version = tuple(map(int, sys.argv[2]))
     else:
         version = (2, 7, 0, 0)  # the latest SDK version
-
-    dsock = rx.mkdatasock()
+    
+    # get the IP address of eth0
+    dsock = rx.mkdatasock(ip_address=get_ip_address('eth0'))
     count = 0
     while count < max_count:
         data = dsock.recv(rx.MAX_PACKETSIZE)
         packet = rx.unpack(data, version=version)
         if type(packet) is rx.SenderData:
             version = packet.natnet_version
-            print("NatNet version received:", version)
+            print('NatNet version received:', version)
         if type(packet) in [rx.SenderData, rx.ModelDefs, rx.FrameOfData]:
-            print(dumps(packet._asdict(), indent=4))
+          print('Position: ' + str(packet.rigid_bodies[0].position))
+          print('Position: ' + str(packet.rigid_bodies[0].orientation))
+          print('Timestamp: %f' % packet.timestamp)
+          print('')
+          #~ print(dumps(packet._asdict(), indent=4))
         count += 1
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     demo_recv_data()
