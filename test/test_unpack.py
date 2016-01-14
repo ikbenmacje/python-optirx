48ba8470e5210982dd5d51709baa33eb0a046f24
Garth Zeglin <garthz@cmu.edu>
2016-01-14 15:01:53 -0500

    Adding test case for Motive 1.9/NatNet 2.9 data.

--- test/test_unpack.py
+++ test/test_unpack.py
@@ -98,5 +98,32 @@ def test_unpack_frame_of_data_natnet2700():
             assert_equal(parsed.skeletons, [])
             assert_equal(len(parsed.labeled_markers), 3)
 
+def test_unpack_frame_of_data_natnet2900():
+
+    expected_rb = [
+        (-0.309438, 0.311235,  0.201755),
+        (-0.202528, 0.266192,  0.364530),
+        (-0.197157, 0.263633,  0.114435) ]
+
+    expected_om = [
+        ( 3.146829, 2.173529,  0.359759)]
+
+    for i in range(1,1+1):
+        with open("test/data/frame-motive-1.9.0-%03d.bin" % i, "rb") as f:
+            binary = f.read()
+            parsed = rx.unpack(binary, (2,9,0,0))
+            print(parsed)
+            assert_is(type(parsed), rx.FrameOfData)
+            assert_in(parsed.frameno, [9764967, 9764968])
+            assert_in(b"all", parsed.sets)
+            assert_in(b"Triangle", parsed.sets)
+            assert_almost_equal(parsed.sets[b"Triangle"], expected_rb, 4)
+            assert_equal(parsed.rigid_bodies[0].mrk_ids, (1,2,3))
+            assert_equal(len(parsed.other_markers), 1)
+            assert_almost_equal(parsed.other_markers, expected_om, 1)
+            assert_equal(parsed.skeletons, [])
+            assert_equal(len(parsed.labeled_markers), 4)
+
+
 
 # TODO: create a test with latency and timestamp data
